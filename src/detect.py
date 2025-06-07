b7b23b590e435aa952c49f612c7ff94339cd59f9
KamSaf <bopaf42@gmail.com>
2025-06-07 12:11:50 +0200

    Add docstring to detect.py

--- src/detect.py
+++ src/detect.py
@@ -7,7 +7,23 @@ def predict(
     img_path: str,
     weights: str = "runs/detect/train/weights/best.pt",
     show_result: bool = False,
-) -> list | None:
+) -> tuple[float, float, float, float] | None:
+    """
+    Function predicting coordinates of the license plate
+    bounding box on given image.
+
+    Parameters:
+        img_path (str): path to image which is to be processed
+
+        weights (str): path to YOLO11n weights (optional)
+
+        show_result (bool): bool flag determining whether image with marked bbox should be displayed
+
+    Returns:
+        result (tuple[float, float, float, float] | None):
+        tuple with bbox coordinates or None if bbox was not found
+
+    """
     model = YOLO(weights)
     results = model.predict(source=img_path, verbose=False)[0]
     if not results:
